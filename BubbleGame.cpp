4b0fdc2fb6b6d169a035f1933be55b7a6d48f302
Johnny Xiao <yuxiao9282@gmail.com>
2025-12-02 19:39:07 -0800

    Initial SFML bubble game version

---
+++ BubbleGame.cpp
@@ -0,0 +1,95 @@
+#include <SFML/Graphics.hpp>
+#include <ctime>
+#include <cstdlib>
+
+class Game {
+private:
+    sf::RenderWindow window;
+    sf::CircleShape player;
+    sf::RectangleShape target;
+    sf::Font font;
+    sf::Text scoreText;
+
+    float speed = 0.5f; // Further reduce speed for smoother movement
+    int score = 0;
+
+public:
+    Game() 
+        : window(sf::VideoMode(800, 600), "Simple SFML Game") 
+    {
+        std::srand(std::time(nullptr));
+
+        // Player
+        player.setRadius(25.f);
+        player.setFillColor(sf::Color::Blue);
+        player.setPosition(400, 300);
+
+        // Target
+        target.setSize(sf::Vector2f(30, 30));
+        target.setFillColor(sf::Color::Red);
+        randomizeTargetPosition();
+
+        // Font
+        font.loadFromFile("/System/Library/Fonts/Supplemental/Arial.ttf");
+
+        scoreText.setFont(font);
+        scoreText.setCharacterSize(24);
+        scoreText.setFillColor(sf::Color::White);
+        scoreText.setPosition(10, 10);
+    }
+
+    void run() {
+        while (window.isOpen()) {
+            handleEvents();
+            update();
+            render();
+        }
+    }
+
+private:
+    void handleEvents() {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Movement
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            player.move(-speed, 0);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            player.move(speed, 0);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            player.move(0, -speed);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            player.move(0, speed);
+    }
+
+    void update() {
+        // Collision detection
+        if (player.getGlobalBounds().intersects(target.getGlobalBounds())) {
+            score++;
+            randomizeTargetPosition();
+        }
+
+        scoreText.setString("Score: " + std::to_string(score));
+    }
+
+    void render() {
+        window.clear(sf::Color::Black);
+        window.draw(player);
+        window.draw(target);
+        window.draw(scoreText);
+        window.display();
+    }
+
+    void randomizeTargetPosition() {
+        target.setPosition(std::rand() % 770, std::rand() % 570);
+    }
+};
+
+int main() {
+    Game game;
+    game.run();
+    return 0;
+}
